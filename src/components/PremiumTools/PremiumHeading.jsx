d12e02eca1fec9513f1d77bf4ed91e9519fe5185
Ahsanul-Islam-083 <ahsanulislam083@gmail.com>
2026-03-30 11:43:46 +0600

    displayed dynamic data in the toolsCard

--- src/components/PremiumTools/PremiumHeading.jsx
+++ src/components/PremiumTools/PremiumHeading.jsx
@@ -6,7 +6,7 @@ const PremiumHeading = ({toolsPromise}) => {
     // console.log(tools);
     
     return (
-        <div className='container mx-auto py-24'>
+        <div className='container mx-auto pt-6'>
             <h2 className='text-4xl font-bold text-center'>Premium Digital Tools</h2>
             <p className='text-center text-gray-400 py-8'>Choose from our curated collection of premium digital products designed to boost your productivity and creativity.</p>
             <ToolsTab tools={tools}/>
--- src/components/PremiumTools/ToolsCard.jsx
+++ src/components/PremiumTools/ToolsCard.jsx
@@ -5,19 +5,6 @@ const ToolsCard = ({ tool }) => {
 
     return (
         <div className='border border-gray-200 rounded-2xl p-4 shadow transform transition duration-300 hover:scale-105 relative'>
-            {/* {model.status && (
-                <div
-                    className={`absolute top-4 right-4 px-4 py-1.5 text-xs font-semibold rounded-full uppercase tracking-wider
-                                        ${model.status === "popular" ? "bg-red-600 text-white" : ""}
-                                        ${model.status === "favourite" ? "bg-orange-500 text-white" : ""}
-                                        ${model.status === "mostwanted" ? "bg-amber-500 text-white" : ""}
-                                    `}
-                >
-                    {model.status === "popular" && "🔥 Popular"}
-                    {model.status === "favourite" && "❤️ Favourite"}
-                    {model.status === "mostwanted" && "⭐ Most Wanted"}
-                </div>
-            )} */}
 
             {
                 tool.tagType && (
@@ -34,16 +21,16 @@ const ToolsCard = ({ tool }) => {
                 <div className='p-4 border border-gray-200 rounded-full'><img className='h-10' src={tool.icon} alt="" /></div>
             </div>
             <div>
-                <h3 className='text-2xl font-bold'>AI Writing Pro</h3>
-                <p className=' text-gray-400 py-4'>Generate high-quality content, blogs, and marketing copy in seconds with advanced AI.</p>
+                <h3 className='text-2xl font-bold'>{tool.name}</h3>
+                <p className=' text-gray-400 py-4'>{tool.description}</p>
                 <h3 className='text-2xl font-bold pb-4'>
-                    $29
-                    <span className='text-[16px] text-gray-400 font-normal'>/month</span>
+                    ${tool.price}
+                    <span className='text-[16px] text-gray-400 font-normal'>/{tool.period}</span>
                 </h3>
                 <ul class="space-y-3 mb-8 text-gray-400">
-                    <li class="flex items-center gap-2"><span className='text-green-400'>✔</span> Everything in Pro</li>
-                    <li class="flex items-center gap-2"><span className='text-green-400'>✔</span> Team collaboration</li>
-                    <li class="flex items-center gap-2"><span className='text-green-400'>✔</span> Custom integrations</li>
+                    {
+                        tool.features.map((f,i)=> <li key={i} class="flex items-center gap-2"><span className='text-green-400'>✔</span> {f}</li>)
+                    }
                 </ul>
                 <button className='btn w-full bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white rounded-full'>Buy Now</button>
             </div>
